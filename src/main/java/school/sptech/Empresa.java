21f7e7a51485ca831a9a14c1aa216ca18c44a109
SamuelAntunes2006 <samuel.asantos@sptech.school>
2025-10-22 14:13:28 -0300

    Subindo prova

--- src/main/java/school/sptech/Empresa.java
+++ src/main/java/school/sptech/Empresa.java
@@ -1,5 +1,63 @@
 package school.sptech;
 
-public class Empresa {
+import school.sptech.exception.ArgumentoInvalidoException;
+import school.sptech.exception.JogoInvalidoException;
+import school.sptech.exception.JogoNaoEncontradoException;
 
+import java.time.LocalDate;
+import java.util.List;
+
+public class Empresa {
+    public String nome;
+    List<Jogo> jogos;
+
+    public Empresa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // adicionar
+
+   public void adicionarJogo(Jogo jogo){
+        jogos.add(jogo);
+   }
+
+
+    // remover
+    public void removerJogoPorCodigo(String codigo){
+            if (codigo != null) {
+                for (int i = 0; i < jogos.size();i++) {
+                    for(int j =0; j < codigo.length();j++) {
+                        if(codigo.equals(codigo)){
+                            jogos.remove(i);
+                        }
+                    }
+                }
+            }
+    }
+
+
+    public List<Jogo> buscarJogoPorPeriodo(LocalDate dataInicio, LocalDate dataFim){
+            for(int i =0; i< jogos.size();i++){}
+        return List.of();
+    }
+
+// buscar
+    public String buscarJogoPorCodigo(String codigo) {
+        for (int i = 0; i<jogos.size();i++) {
+                     return null;
+                    }
+        return codigo;
+    }
+
+    public Jogo buscarJogoComMelhorAvaliacao(){
+        return null;
+    }
 }
+
